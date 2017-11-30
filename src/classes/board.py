7d8f5c1a8a598bd70fab70e69f76492791377923
Dave Buitenhuis <dave@Daves-MBP.lan>
2017-11-30 02:02:48 +0100

    Added board creation

--- src/classes/board.py
+++ src/classes/board.py
@@ -1,10 +1,19 @@
-import config
-
 class Board:
 
-    def __init__(self):
+    def __init__(self, size):
+        
+        self.generate_board_layout(size)
 
-        # TODO
+
+    def generate_board_layout(self, size):
+        
+        self.set_board_layout([[new Block]*size for i in range(size)])
+        
+    
+    def set_board_layout(self, boardLayout):
+        
+        self.boardLayout = boardLayout
+
 
 
         
